ed1d76e4f561c24b6a62478f80d1c84287f9749f
donaderoyan <donaderoyan@gmail.com>
2024-05-13 11:39:37 +0700

    New setup docker and environments

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	helmet "github.com/danielkov/gin-helmet"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+
+	util "github.com/donaderoyan/talentgrowth-be/utils"
+)
+
+func main() {
+	r := SetupRouter()
+
+	log.Fatal(r.Run(":" + util.GodotEnv("GO_PORT")))
+}
+
+func SetupRouter() *gin.Engine {
+	router := gin.Default()
+
+	if util.GodotEnv("GO_ENV") != "production" && util.GodotEnv("GO_ENV") != "test" {
+		gin.SetMode(gin.DebugMode)
+	} else if util.GodotEnv("GO_ENV") == "test" {
+		gin.SetMode(gin.TestMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"*"},
+		AllowHeaders:  []string{"*"},
+		AllowWildcard: true,
+	}))
+	router.Use(helmet.Default())
+	router.Use(gzip.Gzip(gzip.BestCompression))
+
+	groupRoute := router.Group("/api/v1")
+	groupRoute.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	return router
+}
